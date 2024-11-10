5c1d26ed00e223fce8bf1c9b4d6c8c1b17e3ac3e
Shou-Chih Lo <sclo@gms.ndhu.edu.tw>
2024-11-10 23:21:04 +0800

    Add files via upload
    initial commit

---
+++ BannerTestStarter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BannerTestStarter
+//
+//  Created by 羅壽之 on 2023/3/28.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    let banner_images = ["photo0_0", "photo0_1", "photo1_0", "photo1_1", "photo1_2", "photo2_0", "photo2_1"]
+    
+    var body: some View {
+        Text("Get ready")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
